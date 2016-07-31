e641421330e14dc13228c8252bbab7b6b119dbc7
radeshshiwlochan <radesh0430@gmail.com>
2016-07-31 12:00:11 -0400

    adds basicRouteAdvance.js, shows how to use GET routes

--- basicRoutes/basicRoute.js
+++ basicRoutes/basicRoute.js
@@ -7,6 +7,13 @@ function router(req, res) {
 	res.end('<h1> Hello Router</h1>');
 }
 
+/*
+ router is a function that is passed to the built in function,
+ createServer. It is passed in the createServer method without 
+ being called hence it being used as "router " instead of 
+ "router()" as calling it with the parameter as in a function
+ call would mean it would be invoked when passed.
+*/
 http.createServer(router).listen(3000, () => {
 	console.log('listening on port 3000');
 });
